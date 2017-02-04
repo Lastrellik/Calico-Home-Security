6940b233389c6bd882de9ae3e622fa75b5dd8d16
Jason Bruderer <Lastrellik@gmail.com>
2017-02-03 21:15:42 -0700

    Added general comments to the top of two classes

--- src/module_TOUCHSCREEN/TouchscreenBackEnd.java
+++ src/module_TOUCHSCREEN/TouchscreenBackEnd.java
@@ -1,3 +1,11 @@
+/**
+ * Salt Lake Community College
+ * Spring 2017 Semester
+ * CSIS-2810 Computer Architecture
+ * Instructor: Jon McGowan
+ * 
+ */
+
 package module_TOUCHSCREEN;
 
 public class TouchscreenBackEnd {
--- src/module_TOUCHSCREEN/TouchscreenGUI.java
+++ src/module_TOUCHSCREEN/TouchscreenGUI.java
@@ -1,3 +1,10 @@
+/**
+ * Salt Lake Community College
+ * Spring 2017 Semester
+ * CSIS-2810 Computer Architecture
+ * Instructor: Jon McGowan
+ * 
+ */
 package module_TOUCHSCREEN;
 
 import java.awt.BorderLayout;
