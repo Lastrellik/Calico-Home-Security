f3ef530f329d566c191569401b98ee1c9d765379
Jason Bruderer <Lastrellik@gmail.com>
2017-02-03 21:09:54 -0700

    Added the framework for the touchscreen module

---
+++ src/module_TOUCHSCREEN/README.md
@@ -0,0 +1,11 @@
+The touchscreen module is a way for the user to 
+arm and monitor the security system remotely. It 
+is through this application that we can arm/disarm 
+the security system as well as add functionality 
+such as taking pictures of the intruder and sending 
+emails in the event of a breach. 
+
+This application is designed to run on a 7'' Raspberry 
+Pi Touchscreen Display such as the one listed here: 
+
+https://www.amazon.com/Raspberry-Pi-7-Touchscreen-Display/dp/B0153R2A9I/ref=sr_1_2?s=pc&ie=UTF8&qid=1486180399&sr=1-2&keywords=raspberry+pi+touchscreen
---
+++ src/module_TOUCHSCREEN/TouchscreenGUI.java
@@ -0,0 +1,46 @@
+package module_TOUCHSCREEN;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class TouchscreenGUI extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TouchscreenGUI frame = new TouchscreenGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TouchscreenGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
