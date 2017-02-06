96c746914426212683e93bde1cef491b6130d1e8
Jason Bruderer <Lastrellik@gmail.com>
2017-02-06 02:00:32 -0700

    Test serial Rx Tx communication

--- src/module_TOUCHSCREEN/TouchscreenBackEnd.java
+++ src/module_TOUCHSCREEN/TouchscreenBackEnd.java
@@ -8,6 +8,25 @@
 
 package module_TOUCHSCREEN;
 
-public class TouchscreenBackEnd {
+import com.fazecast.jSerialComm.*;
 
+public class TouchscreenBackEnd {
+	public static void main(String[] args) throws InterruptedException{
+		SerialPort comPort = SerialPort.getCommPorts()[1];
+		byte[] readBuffer;
+		comPort.openPort();
+		comPort.setComPortTimeouts(SerialPort.TIMEOUT_NONBLOCKING, 0, 0);
+		try {
+		   while (true)
+		   {
+			  comPort.writeBytes(new byte[] {1, 2, 3, 4, 5, 6}, 1);
+		      Thread.sleep(1000);
+		      readBuffer = new byte[comPort.bytesAvailable()];
+		      int numRead = comPort.readBytes(readBuffer, readBuffer.length);
+		      System.out.println("Read " + numRead + " bytes.");
+		      System.out.println(new String(readBuffer));
+		   }
+		} catch (Exception e) { e.printStackTrace(); }
+		comPort.closePort();
+	}
 }
