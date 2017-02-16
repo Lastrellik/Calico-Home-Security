37e6cf3d75c23d1c67e8a56851a1b44310627b6f
Tyler <jacobstyler2358@gmail.com>
2017-02-15 20:28:55 -0700

    basic flash

--- src/main.cpp
+++ src/main.cpp
@@ -1,31 +1,56 @@
-/**
- * Calico Home Security System
- *
- *
- */
 #include "Arduino.h"
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 13
-#endif
+int ledG = 9;
+int ledY = 8;
+int ledR = 7;
+int photoR = 0;
+long previousMillis = 0;
+long intervalA = 1000;
+long intervalB = 1500;
+int state1 = LOW;
+int state2 = LOW;
 
-void setup()
-{
-  // initialize LED digital pin as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+void setup() {
+
+  pinMode(ledG, OUTPUT);
+  pinMode(ledY, OUTPUT);
+  pinMode(ledR, OUTPUT);
+  pinMode(photoR, INPUT);
+  Serial.begin(9600);  //Begin serial communcation
 }
 
-void loop()
-{
-  // turn the LED on (HIGH is the voltage level)
-  digitalWrite(LED_BUILTIN, HIGH);
+void loop(){
 
-  // wait for a second
-  delay(1000);
 
-  // turn the LED off by making the voltage LOW
-  digitalWrite(LED_BUILTIN, LOW);
+  unsigned long currentMillis = millis();
+  long check = currentMillis - previousMillis;
+  Serial.println(check);
 
-   // wait for a second
-  delay(1000);
+  if(analogRead(photoR) < 150){
+      digitalWrite(ledG, HIGH);
+      digitalWrite(ledR, LOW);
+      digitalWrite(ledY, LOW);
+
+
+
+  } else{
+      digitalWrite(ledG, LOW);
+      if (currentMillis - previousMillis >= intervalA) {
+          // save the last time you blinked the LED
+          previousMillis = currentMillis;
+
+          // if the LED is off turn it on and vice-versa:
+          if (state1 == LOW){
+            state1 = HIGH;
+            state2 = LOW;
+          }
+          else{
+            state1 = LOW;
+            state2 = HIGH;
+          }
+          // set the LED with the ledState of the variable:
+          digitalWrite(ledR, state1);
+          digitalWrite(ledY, state2);
+    }
+  }
 }
