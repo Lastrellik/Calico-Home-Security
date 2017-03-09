e45764a14ccb728c96c3bd8e71d85249f6175cfc
Lastrellik <lastrellik@gmail.com>
2017-03-08 21:20:37 -0700

    Convert to Sha1 from Sha256

--- src/DataPacket.h
+++ src/DataPacket.h
@@ -5,14 +5,17 @@
 #define DataPacket_h
 
 #include "Arduino.h"
-#include "../lib/Cryptosuite/Sha/sha256.h"
+#include "../lib/Cryptosuite/Sha/sha1.h"
+
+#define SIZE_OF_SHA1_IN_BYTES 20
+#define SIZE_OF_DATAPACKET_IN_BYTES 64
 
 class DataPacket{
     public:
       DataPacket(String message, String packetType);
       DataPacket(byte* packetContents);
       byte* getPacketAsArray();
-      byte* getPacketSha256Hash();
+      byte* getPacketSha1Hash();
       int getSizeInBytes();
       DataPacket getDataPacket();
     private:
@@ -22,12 +25,12 @@ class DataPacket{
       void appendMessage();
       void append(byte rawData[]);
       void append(byte singleRawByte);
-      void parseSha256FromRawPacket(byte* packetContents);
+      void parseSha1FromRawPacket(byte* packetContents);
       void parsePacketTypeFromRawPacket(byte* packetContents);
       void parseMessageFromRawPacket(byte* packetContents);
       int arrayCounter = 1;
-      byte sha256MessageHash[32];
-      byte packetAsArray[64] { '~' };
+      byte sha1MessageHash[SIZE_OF_SHA1_IN_BYTES];
+      byte packetAsArray[SIZE_OF_DATAPACKET_IN_BYTES] { '~' };
       String messageString;
       String packetTypeString;
 };
