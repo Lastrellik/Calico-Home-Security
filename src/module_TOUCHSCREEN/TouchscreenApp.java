4e87ffff5e35a8ce4f721c7624f7b0fc5ce079f2
Tyler <jacobstyler2358@gmail.com>
2017-04-05 10:10:36 -0600

    Working on fixing continual output for serial commands.

--- src/module_TOUCHSCREEN/TouchscreenApp.java
+++ src/module_TOUCHSCREEN/TouchscreenApp.java
@@ -17,7 +17,7 @@ public class TouchscreenApp {
 	public static void main(String[] args){
 		
 		JFrame frame = new JFrame("FrameDemo");
-		frame.setSize(800,525);
+		frame.setSize(820,525);
 		MainPanel panel = new MainPanel();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.add(panel);
