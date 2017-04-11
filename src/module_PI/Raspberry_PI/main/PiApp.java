5c098b5128e025ac0b4bcd6896f13518999218a0
Jason Bruderer <Lastrellik@gmail.com>
2017-04-11 00:59:38 -0600

    Switch to Command Line Arguments for Credential System and Fix Minor Errors
    	modified:   src/module_PI/Raspberry_PI/JUnitTests/EmailNotifierTest.java
    	modified:   src/module_PI/Raspberry_PI/main/EmailNotifier.java
    	modified:   src/module_PI/Raspberry_PI/main/PiApp.java

--- src/module_PI/Raspberry_PI/main/PiApp.java
+++ src/module_PI/Raspberry_PI/main/PiApp.java
@@ -1,5 +1,7 @@
 package module_PI.Raspberry_PI.main;
 
+import java.io.*;
+import java.util.*;
 
 public class PiApp {
 
@@ -7,31 +9,38 @@ public class PiApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		parseArgs(args);
 		SerialInputListener inputListener = new SerialInputListener();
 		Thread listener = new Thread(inputListener, "Listener");
 		listener.start();		
 		SerialOutput output = new SerialOutput();
+    	EmailNotifier notifier = new EmailNotifier("lastrellik@gmail.com", "Today's message");
+    	notifier.sendEmail();
 		
 		pause(10000);
-		output.sendTestComponentsPacket();
-		output.sendCalibratePacket();
 		output.sendArmPacket();
-		output.sendTriggerPacket();
-		output.sendSilencePacket();
-		
-		while(true){
-			output.sendArmPacket();
-			pause(5000);
-			output.sendTriggerPacket();
-			pause(2000);
-			output.sendSilencePacket();
-			pause(2000);
-			output.sendDisarmPacket();
-			pause(5000);
-			output.sendResetCalibrationPacket();
-			pause(5000);
-			output.sendCalibratePacket();
-			pause(5000);
+	}
+	
+	private static void parseArgs(String[] args){
+		int numOfArguments = 2;
+		if(args.length != numOfArguments) throw new IllegalArgumentException("Syntax is java PiApp <username> <password>");
+		parseGmailCredentials(args[0], args[1]);
+	}
+	
+	private static void parseGmailCredentials(String username, String password){
+		Properties gmailCredentials = new Properties();
+		gmailCredentials.put("gmailUsername" , username);
+		gmailCredentials.put("gmailPassword" , password);
+		saveGmailProperties(gmailCredentials);
+	}
+	
+	private static void saveGmailProperties(Properties gmailCredentials){
+		try {
+			FileOutputStream out = new FileOutputStream("gmailCredentials");
+			gmailCredentials.store(out, "Gmail Credentials");
+			out.close();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 	
