e197295f72c1b4296205037241c8fa2261949649
Jason Bruderer <Lastrellik@gmail.com>
2017-04-15 21:44:07 -0600

    Create PacketProcessor

--- src/module_PI/Raspberry_PI/main/PiApp.java
+++ src/module_PI/Raspberry_PI/main/PiApp.java
@@ -5,34 +5,32 @@ import java.util.*;
 public class PiApp {
 	public static Properties GMAIL_CREDENTIALS = new Properties();
 	private static ActivityLogger activityLogger = new ActivityLogger("PI_Log.txt");
+	private static String[] emailRecipients;
 
 	/**
 	 * @param args Gmail Username and Password for sending mail
 	 */
 	public static void main(String[] args) {
 		int numOfArgs = 2;
-		if(args.length != numOfArgs) throw new IllegalArgumentException("Syntax is java PiApp <gmailusername> <gmailpassword>");
+		if(args.length <= numOfArgs) throw new IllegalArgumentException("Syntax is java PiApp <gmailusername> <gmailpassword> <recipientEmailAddress>...");
 		parseArgs(args);
 		LogToFile("PI startup application");
 		SerialInputListener inputListener = new SerialInputListener();
+		PacketProcessor packetProcessor = new PacketProcessor(inputListener, emailRecipients);
 		Thread listener = new Thread(inputListener, "Listener");
-		listener.start();		
-		SerialOutput output = new SerialOutput();
-		
-		pause(10000);
-		output.sendArmPacket();
+		Thread processor = new Thread(packetProcessor, "Processor");
+		listener.start();
+		processor.start();
 	}
 	
 	private static void parseArgs(String[] args){
+		int numOfRecipients = args.length - 2;
+		int recipientIndex = 0;
+		emailRecipients = new String[numOfRecipients];
 		GMAIL_CREDENTIALS.put("gmailUsername" , args[0]);
 		GMAIL_CREDENTIALS.put("gmailPassword" , args[1]);
-	}
-	
-	private static void pause(int millis){
-		try {
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
+		for(int i = 2; i < args.length; i++){
+			emailRecipients[recipientIndex++] = args[i];
 		}
 	}
 	
@@ -41,7 +39,7 @@ public class PiApp {
 	}
 	
 	public static void LogToFile(String messageToLog){
-		activityLogger.log("\t\t\t\t" + messageToLog);
+		activityLogger.log(messageToLog);
 	}
 
 }
